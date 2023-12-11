4fc591d254ec2b5169f6e989406769e969d18ef4
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-12-11 14:20:03 +0530

    fixes some issue

--- middlewares/not_found.js
+++ middlewares/not_found.js
@@ -1,7 +1,7 @@
 
 
 export const notFoundErrorHandler = (req, res, next)=> {
-    res.send({msg:`Invalid Path: ${req.originalUrl}.Please check our documentation for more information at: <a href="https://social-media-bhh7.onrender.com/api/docs">Documenttation</a>`})
+    res.send(`<p>Invalid Path: ${req.originalUrl}.Please check our documentation for more information at: <a href="https://social-media-bhh7.onrender.com/api/docs">Documenttation</a></p>`);
 
     /* res.status(404).json({msg:`Invalid Path: ${req.originalUrl}.Please check our documentation for more information at: /api/docs`}); */
 }
